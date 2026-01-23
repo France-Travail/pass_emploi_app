f9f7e57de4e631d2ea9ceacd576adfe450d4a3f4
Alwein <adrien.cens@edu.esiee.fr>
2026-01-23 09:49:51 +0100

    feat(analytics): add tracking for "Mes outils" interactions

--- lib/widgets/mes_outils_card.dart
+++ lib/widgets/mes_outils_card.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:pass_emploi_app/analytics/analytics_constants.dart';
 import 'package:pass_emploi_app/pages/boite_a_outils_page.dart';
 import 'package:pass_emploi_app/ui/app_icons.dart';
 import 'package:pass_emploi_app/ui/margins.dart';
 import 'package:pass_emploi_app/ui/strings.dart';
 import 'package:pass_emploi_app/ui/text_styles.dart';
+import 'package:pass_emploi_app/utils/pass_emploi_matomo_tracker.dart';
 import 'package:pass_emploi_app/widgets/cards/generic/card_container.dart';
 
 class MesOutilsCard extends StatelessWidget {
@@ -12,7 +14,13 @@ class MesOutilsCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CardContainer(
-      onTap: () => Navigator.push(context, BoiteAOutilsPage.materialPageRoute()),
+      onTap: () {
+        PassEmploiMatomoTracker.instance.trackEvent(
+          eventCategory: AnalyticsEventNames.mesOutilsCategory,
+          action: AnalyticsEventNames.mesOutilsAction,
+        );
+        Navigator.push(context, BoiteAOutilsPage.materialPageRoute());
+      },
       child: Row(
         children: [
           Image.asset(
