c7d7ff04fad60769cf9fad70e71341d11313b1e6
Alwein <adrien.cens@edu.esiee.fr>
2026-06-15 14:54:14 +0200

    feat(tutorial): update tutorial content and version timestamp

--- lib/models/tutorial/tutorial.dart
+++ lib/models/tutorial/tutorial.dart
@@ -1,22 +1,22 @@
 import 'package:pass_emploi_app/models/tutorial/tutorial_page.dart';
 
 class Tutorial {
-  static const String versionTimestamp = '1718704184';
+  static const String versionTimestamp = '1718704185';
   static const String ftIaTutorialVersionTimestamp = 'ia_1';
 
   static List<TutorialPage> milo = [
     TutorialPage(
-      title: "Suivez facilement vos candidatures\u{00A0}!",
+      title: "Le mode sombre est disponible\u{00A0}!",
       description:
-          'En cliquant sur "Je postule" depuis une offre, indiquez si vous avez candidaté et suivez cette information à tout moment dans le nouvel onglet "Suivi des offres".',
+          "Rendez-vous dans votre profil, rubrique Apparence, pour l'activer. Une expérience visuelle encore plus confortable.",
       image: "assets/tuto/tuto.webp",
     ),
   ];
   static List<TutorialPage> pe = [
     TutorialPage(
-      title: "Suivez facilement vos candidatures\u{00A0}!",
+      title: "Le mode sombre est disponible\u{00A0}!",
       description:
-          'En cliquant sur "Je postule" depuis une offre, indiquez si vous avez candidaté et suivez cette information à tout moment dans le nouvel onglet "Suivi des offres".',
+          "Rendez-vous dans votre profil, rubrique Apparence, pour l'activer. Une expérience visuelle encore plus confortable.",
       image: "assets/tuto/tuto.webp",
     ),
   ];
