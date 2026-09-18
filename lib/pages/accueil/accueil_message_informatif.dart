9ca9c3b1c7f019a049f921b146a410117505cd53
Alwein <adrien.cens@edu.esiee.fr>
2026-09-18 18:55:02 +0200

    style : message accueil

--- lib/pages/accueil/accueil_message_informatif.dart
+++ lib/pages/accueil/accueil_message_informatif.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dsfr/flutter_dsfr.dart';
 import 'package:pass_emploi_app/models/accueil/message_informatif.dart';
-import 'package:pass_emploi_app/ui/app_colors.dart';
-import 'package:pass_emploi_app/ui/app_icons.dart';
-import 'package:pass_emploi_app/ui/margins.dart';
-import 'package:pass_emploi_app/ui/text_styles.dart';
 import 'package:pass_emploi_app/utils/launcher_utils.dart';
 import 'package:pass_emploi_app/utils/pass_emploi_matomo_tracker.dart';
 import 'package:pass_emploi_app/utils/platform.dart';
-import 'package:pass_emploi_app/widgets/buttons/primary_action_button.dart';
-import 'package:pass_emploi_app/widgets/cards/generic/card_container.dart';
 
 class AccueilMessageInformatif extends StatelessWidget {
   final MessageInformatif messageInformatif;
@@ -18,38 +13,44 @@ class AccueilMessageInformatif extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final cta = messageInformatif.cta;
-    return CardContainer(
-      backgroundColor: AppColors.primaryLighten,
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Icon(AppIcons.info_rounded, color: AppColors.primary),
-          SizedBox(width: Margins.spacing_s),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(messageInformatif.titre, style: TextStyles.textSBold.copyWith(color: AppColors.primary)),
-                SizedBox(height: Margins.spacing_s),
-                Text(messageInformatif.contenu, style: TextStyles.textSRegular().copyWith(color: AppColors.primary)),
-                if (cta != null) ...[
-                  SizedBox(height: Margins.spacing_s),
-                  PrimaryActionButton(
-                    heightPadding: Margins.spacing_s,
-                    icon: Icons.download,
-                    label: cta.label,
-                    onPressed: () {
-                      final url = cta.url(isAndroid: PlatformUtils.getPlatform.isAndroid);
-                      PassEmploiMatomoTracker.instance.trackOutlink(url);
-                      launchExternalUrl(url);
-                    },
-                  ),
-                ],
-              ],
-            ),
-          ),
-        ],
-      ),
+    return DsfrAlert(
+      type: DsfrAlertType.info,
+      title: messageInformatif.titre,
+      description: cta == null
+          ? DsfrAlertDescriptionText(messageInformatif.contenu)
+          : DsfrAlertDescriptionWidget(_DescriptionWithCta(contenu: messageInformatif.contenu, cta: cta)),
+    );
+  }
+}
+
+class _DescriptionWithCta extends StatelessWidget {
+  final String contenu;
+  final MessageInformatifCta cta;
+
+  const _DescriptionWithCta({required this.contenu, required this.cta});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          contenu,
+          style: DsfrTextStyle.bodyMd(color: DsfrColorDecisions.textDefaultGrey(context)),
+        ),
+        const SizedBox(height: DsfrSpacings.s2w),
+        DsfrButton(
+          label: cta.label,
+          icon: DsfrIcons.systemDownloadLine,
+          variant: DsfrButtonVariant.secondary,
+          size: DsfrComponentSize.sm,
+          onPressed: () {
+            final url = cta.url(isAndroid: PlatformUtils.getPlatform.isAndroid);
+            PassEmploiMatomoTracker.instance.trackOutlink(url);
+            launchExternalUrl(url);
+          },
+        ),
+      ],
     );
   }
 }
